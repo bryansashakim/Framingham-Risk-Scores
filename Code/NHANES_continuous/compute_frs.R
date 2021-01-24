ca7207842eb5e929a49517c7c22bb06aa755a6ae
bryansashakim <Bryankim776@gmail.com>
2021-01-23 22:45:26 -0800

    edit tabs

--- Code/NHANES_continuous/compute_frs.R
+++ Code/NHANES_continuous/compute_frs.R
@@ -70,3 +70,80 @@ get_frs = function(riagendr,ridageyr,HDL,TotChol,sbp,smoker,diabetic) {
   }
   return(sum(list_pts))
 }
+
+
+##############
+#### ATP3 ####
+##############
+
+f_points=data.frame("points"=seq(-3,12),
+                    "age_low"=c(NA,NA,NA,30,NA,35,NA,40,45,NA,50,55,60,65,70,75),
+                    "age_high"=c(NA,NA,NA,34,NA,39,NA,44,49,NA,54,59,64,69,74,120),
+                    "HDL_low"=c(NA,60,50,45,35,0,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA),
+                    "HDL_high"=c(NA,1000,59,49,44,35,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA),
+                    "TotChol_low"=c(NA,NA,NA,0,160,NA,200,240,280,NA,NA,NA,NA,NA,NA,NA),
+                    "TotChol_high"=c(NA,NA,NA,160,199,NA,239,279,1000,NA,NA,NA,NA,NA,NA,NA), 
+                    "SBP_low"=c(NA,NA,0,NA,NA,NA,120,130,NA,140,150,160,NA,NA,NA,NA),
+                    "SBP_high"=c(NA,NA,120,NA,NA,NA,129,139,NA,149,159,1000,NA,NA,NA,NA), 
+                    "Smoker"=c(NA,NA,NA,"No",NA,NA,"Yes",NA,NA,NA,NA,NA,NA,NA,NA,NA), 
+                    "Diabetic"=c(NA,NA,NA,"No",NA,NA,NA,"Yes",NA,NA,NA,NA,NA,NA,NA,NA))
+m_points=data.frame("points"=seq(-2,15),
+                    "age_low"=c(NA,NA,30,NA,35,NA,NA,40,45,NA,50,NA,55,60,65,NA,70,75),
+                    "age_high"=c(NA,NA,34,NA,39,NA,NA,44,49,NA,54,NA,59,64,69,NA,74,120),
+                    "HDL_low"=c(60,50,45,35,0,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA),
+                    "HDL_high"=c(1000,59,49,44,35,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA),
+                    "TotChol_low"=c(NA,NA,0,160,200,240,280,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA),
+                    "TotChol_high"=c(NA,NA,160,199,239,279,1000,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA), 
+                    "SBP_low"=c(NA,NA,0,NA,120,130,140,160,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA),
+                    "SBP_high"=c(NA,NA,120,NA,129,139,149,1000,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA), 
+                    "Smoker"=c(NA,NA,"No",NA,NA,NA,"Yes",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA), 
+                    "Diabetic"=c(NA,NA,"No",NA,NA,"Yes",NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA))
+
+get_frs_atp3 = function(riagendr,ridageyr,HDL,TotChol,sbp,smoker,diabetic) {
+  list_pts=c() # Initiate list of points
+  #### MALES ####
+  if (riagendr == 1) {
+    ## GET POINTS FROM AGE ##
+    i=sapply(ridageyr, function(p) { which(m_points$age_low <= p & m_points$age_high >= p)}); keep_df=m_points[-(setdiff(1:length(m_points$points),i)),];m_age <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]]) 
+    ## GET POINTS FROM HDL ##
+    i=sapply(HDL, function(p) { which(m_points$HDL_low <= p & m_points$HDL_high >= p)}); keep_df=m_points[-(setdiff(1:length(m_points$points),i)),];m_HDL <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]])
+    ## GET POINTS FROM TOTAL CHOLESTEROL ##
+    i=sapply(TotChol, function(p) { which(m_points$TotChol_low <= p & m_points$TotChol_high >= p)}); keep_df=m_points[-(setdiff(1:length(m_points$points),i)),];m_TotChol <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]])
+    ## GET POINTS FROM SBP ##
+    i=sapply(sbp, function(p) { which(m_points$SBP_low <= p & m_points$SBP_high >= p)}); keep_df=m_points[-(setdiff(1:length(m_points$points),i)),];m_SBP <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]])
+    ## GET POINTS FROM SMOKER ##
+    if (isTRUE(smoker == 2)) {list_pts=append(list_pts,0)} else {list_pts=append(list_pts,4)}
+    # m_smoke <<- 4
+    ## GET POINTS FROM DIABETIC ##
+    # if (isTRUE(diabetic < 6.5)) {list_pts=append(list_pts,0)} else {list_pts=append(list_pts,3)}
+    if (isTRUE(diabetic >= 126)) {list_pts=append(list_pts,3)} else {list_pts=append(list_pts,0)}
+    # m_diabetic <<- 3
+  }
+  #### FEMALES ####
+  else {
+    ## GET POINTS FROM AGE ##
+    i=sapply(ridageyr, function(p) { which(f_points$age_low <= p & f_points$age_high >= p)}); keep_df=f_points[-(setdiff(1:length(f_points$points),i)),];f_age <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]]) 
+    ## GET POINTS FROM HDL ##
+    i=sapply(HDL, function(p) { which(f_points$HDL_low <= p & f_points$HDL_high >= p)}); keep_df=f_points[-(setdiff(1:length(f_points$points),i)),];f_HDL <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]])
+    ## GET POINTS FROM TOTAL CHOLESTEROL ##
+    i=sapply(TotChol, function(p) { which(f_points$TotChol_low <= p & f_points$TotChol_high >= p)}); keep_df=f_points[-(setdiff(1:length(f_points$points),i)),];f_TotChol <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]])
+    ## GET POINTS FROM SBP ##
+    i=sapply(sbp, function(p) { which(f_points$SBP_low <= p & f_points$SBP_high >= p)}); keep_df=f_points[-(setdiff(1:length(f_points$points),i)),];f_SBP <<- keep_df[[1]]
+    list_pts=append(list_pts,keep_df[[1]])
+    ## GET POINTS FROM SMOKER ##
+    if (isTRUE(smoker == 2)) {list_pts=append(list_pts,0)} else {list_pts=append(list_pts,3)}
+    # f_smoke <<- 3
+    ## GET POINTS FROM DIABETIC ##
+    # if (isTRUE(diabetic <  6.5)) {list_pts=append(list_pts,0)} else {list_pts=append(list_pts,4)}
+    if (isTRUE(diabetic >= 126)) {list_pts=append(list_pts,4)} else {list_pts=append(list_pts,0)}
+    # f_diabetic <<- 4
+  }
+  return(sum(list_pts))
+}
